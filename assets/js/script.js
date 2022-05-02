77f7dd485cd9f28bf596f32d2aa63c158a55e3d5
Josh-Wickens <joshwickens@hotmail.com>
2022-05-02 15:26:32 +0000

    add basic list of functions and couple of questions to javascript file.

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+function startQuiz() {
+
+}
+
+function nextQuestion() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let questions = [{
+        question: "What is the name of Harry Potters Father?",
+        answers: {
+            a: "John",
+            b: "James",
+            c: "Hargrid"
+        },
+        correctAswer: "b"
+    },
+
+    {
+        question: "What is Harry's favourite spell?",
+        answers: {
+            a: "Avadacadbra",
+            b: "Lumos",
+            c: "Expelliarmus"
+        },
+        correctAswer: "c"
+    },
+
+    {
+        question: "What is the name Hermiones cat?",
+        answers: {
+            a: "Crookshank",
+            b: "Scabbers",
+            c: "Hedwig"
+        },
+        correctAswer: "a"
+    },
+
+    {
+        question: "What does Harry give Dobby to free him?",
+        answers: {
+            a: "Sock",
+            b: "Hat",
+            c: "Wand"
+        },
+        correctAswer: "a"
+    },
+
+]
